e069c64790b111c39d39e93f7cee250872b273f8
mahnoor3650 <mahnoorkhalidhashmi138@gmail.com>
2025-02-21 17:37:07 +0500

    admin dashborad

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/login'
-
+import { ToastContainer, toast } from "react-toastify";
+import { AdminContext } from "./context/adminContext";
+import Navbar from './components/Navbar';
 const App = () => {
-  return (
-    <div>
-      <Login />
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
     </div>
-  )
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App
--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,8 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 export const AdminContext = createContext();
 
 const AdimContextProvider = (props) => {
-  const value = {};
+  const [aToken, setaToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const value = {
+    aToken,
+    setaToken,
+    backendUrl,
+  };
+
   return (
     <AdminContext.Provider value={value}>
       {props.children}
